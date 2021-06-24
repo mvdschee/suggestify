1b46000eb16aec0906e8efde3f3f3bcce7514d7b
Max van der Schee <26795741+mvdschee@users.noreply.github.com>
2021-06-24 15:38:16 +0800

    fixed some stuff

--- api/search.js
+++ api/search.js
@@ -1,16 +1,19 @@
-const LS = require('./levenshtein');
+const { jobs } = require('./mock.json');
 const rateLimit = require('lambda-rate-limiter')({
 	interval: 1000 * 60, // Our rate-limit interval, 1 minute
 	uniqueTokenPerInterval: 500,
 });
 
-const FRUITS = ['Apple', 'Banana', 'Blueberry', 'Cherries'];
+const suggestion = ['Web Developer', 'Graphic Designer', 'Professor', 'Social Worker'];
 const MIN_DISTANCE = 3;
 
 module.exports = async (req, res) => {
-	const { body, headers } = req;
+	const { headers } = req;
+	const body = JSON.parse(req.body);
 
-	const search = sanitize(body.search) || null;
+	console.log('search', body.search);
+
+	const search = body.search ? sanitize(body.search) : null;
 
 	try {
 		await rateLimit.check(50, headers['x-real-ip']);
@@ -18,7 +21,7 @@ module.exports = async (req, res) => {
 		res.status(429).send('Too Many Requests');
 	}
 
-	if (!search) res.status(404).send('No search');
+	if (!search) res.status(200).json(suggestion);
 
 	try {
 		const result = await searchHandler(search);
@@ -31,8 +34,8 @@ module.exports = async (req, res) => {
 function searchHandler(event) {
 	const searchText = event.toLowerCase();
 
-	const filteredFruits = FRUITS.filter((fruit) => {
-		const distance = LS.levenshtein(fruit.toLowerCase(), searchText);
+	const filteredFruits = jobs.filter((job) => {
+		const distance = levenshtein(job.toLowerCase(), searchText);
 		return distance <= MIN_DISTANCE;
 	});
 
@@ -51,4 +54,98 @@ function sanitize(string) {
 	};
 	const reg = /[&<>"'/`]/gi;
 	return string.replace(reg, (match) => map[match]);
+}
+
+// https://github.com/gustf/js-levenshtein
+function levenshtein(a, b) {
+	if (a === b) {
+		return 0;
+	}
+
+	if (a.length > b.length) {
+		const tmp = a;
+		a = b;
+		b = tmp;
+	}
+
+	const la = a.length;
+	const lb = b.length;
+
+	while (la > 0 && a.charCodeAt(la - 1) === b.charCodeAt(lb - 1)) {
+		la--;
+		lb--;
+	}
+
+	const offset = 0;
+
+	while (offset < la && a.charCodeAt(offset) === b.charCodeAt(offset)) {
+		offset++;
+	}
+
+	la -= offset;
+	lb -= offset;
+
+	if (la === 0 || lb < 3) {
+		return lb;
+	}
+
+	const x = 0;
+	let y;
+	let d0;
+	let d1;
+	let d2;
+	let d3;
+	let dd;
+	let dy;
+	let ay;
+	let bx0;
+	let bx1;
+	let bx2;
+	let bx3;
+
+	const vector = [];
+
+	for (y = 0; y < la; y++) {
+		vector.push(y + 1);
+		vector.push(a.charCodeAt(offset + y));
+	}
+
+	const len = vector.length - 1;
+
+	for (; x < lb - 3; ) {
+		bx0 = b.charCodeAt(offset + (d0 = x));
+		bx1 = b.charCodeAt(offset + (d1 = x + 1));
+		bx2 = b.charCodeAt(offset + (d2 = x + 2));
+		bx3 = b.charCodeAt(offset + (d3 = x + 3));
+		dd = x += 4;
+		for (y = 0; y < len; y += 2) {
+			dy = vector[y];
+			ay = vector[y + 1];
+			d0 = _min(dy, d0, d1, bx0, ay);
+			d1 = _min(d0, d1, d2, bx1, ay);
+			d2 = _min(d1, d2, d3, bx2, ay);
+			dd = _min(d2, d3, dd, bx3, ay);
+			vector[y] = dd;
+			d3 = d2;
+			d2 = d1;
+			d1 = d0;
+			d0 = dy;
+		}
+	}
+
+	for (; x < lb; ) {
+		bx0 = b.charCodeAt(offset + (d0 = x));
+		dd = ++x;
+		for (y = 0; y < len; y += 2) {
+			dy = vector[y];
+			vector[y] = dd = _min(dy, d0, dd, bx0, vector[y + 1]);
+			d0 = dy;
+		}
+	}
+
+	return dd;
+}
+
+function _min(d0, d1, d2, bx, ay) {
+	return d0 < d1 || d2 < d1 ? (d0 > d2 ? d2 + 1 : d0 + 1) : bx === ay ? d1 : d1 + 1;
 }
--- src/search.ts
+++ src/search.ts
@@ -5,17 +5,23 @@ interface Options {
 	engine?: string;
 }
 
+interface Cache {
+	[key: string]: Object;
+}
+
 class Search {
 	private root: Element | null;
-	private url?: string;
-	private engine?: string;
+	private engine: string;
+	private url: string;
 	private input?: Element | null;
 	private result?: Element | null;
+	private cache: Cache = {};
+	private timeout = 400;
 
 	constructor(selector: string | Element, options: Options) {
 		this.root = typeof selector === 'string' ? document.querySelector(selector) : selector;
-		this.url = options.url;
-		this.engine = options.engine;
+		this.url = options.url || '/search?q=';
+		this.engine = options.engine || '/api/search';
 		this.input = this.root?.querySelector('input');
 		this.result = this.root?.querySelector('ul');
 
@@ -23,6 +29,13 @@ class Search {
 	}
 
 	initialize(): void {
+		// set icon
+		const i = document.createElement('i');
+		i.classList.add('suggestify-icon');
+		this.root?.insertBefore(i, this.root?.childNodes[0]);
+
+		console.log(this.url);
+
 		if (this.input && this.result) {
 			this.input.setAttribute('role', 'combobox');
 			this.input.setAttribute('autocomplete', 'off');
@@ -34,18 +47,55 @@ class Search {
 			this.input.setAttribute('aria-expanded', 'false');
 
 			this.result.setAttribute('role', 'listbox');
-
 			if (!this.result.id) this.result.id = `suggestify-result-${nanoid(5)}`;
 
 			this.input.setAttribute('aria-owns', this.result.id);
-
 			this.input.addEventListener('input', this.searchHandler, { passive: true });
 		}
 	}
 
-	searchHandler = (e: Event): void => {
-		console.log(e.target.value);
+	searchHandler = (e: any): void => {
+		const text = e.target.value;
+
+		if (this.timeout) clearTimeout(this.timeout);
+		this.timeout = setTimeout(() => {
+			this.request(sanitize(text)).then((response) => {
+				console.log(response);
+			});
+			// update timeout time too
+		}, 400);
 	};
+
+	request = (search: string | null): Promise<any> => {
+		const cacheKey = JSON.stringify(search);
+		if (this.cache[cacheKey]) return Promise.resolve(this.cache[cacheKey]);
+
+		const options = {
+			method: 'POST',
+			body: JSON.stringify({
+				search,
+			}),
+		};
+
+		const result = fetch(this.engine, options).then((response) => response.json());
+		this.cache[cacheKey] = result;
+
+		return result;
+	};
+}
+
+function sanitize(string: string) {
+	const map: any = {
+		'&': '&amp;',
+		'<': '&lt;',
+		'>': '&gt;',
+		'"': '&quot;',
+		"'": '&#x27;',
+		'`': '&grave;',
+		'/': '&#x2F;',
+	};
+	const reg = /[&<>"'/`]/gi;
+	return string.replace(reg, (match) => map[match]);
 }
 
 export default Search;
