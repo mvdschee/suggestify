284c5c72162c1ee648609da8b6fb20d2b57bfa26
Max van der Schee <26795741+mvdschee@users.noreply.github.com>
2021-07-24 22:22:35 +0700

    remove demo, showcase at dist

--- dist/suggestify.umd.js
+++ dist/suggestify.umd.js
@@ -1,7 +1,7 @@
 /*!
 * @project      suggestify
 * @author      	Max van der Schee
-* @build        1627131565239
+* @build        1627140115067
 * @release      1.0.0
 * @copyright    Copyright (c) 2021 Max van der Schee
 */
